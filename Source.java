3c1179aec6a96ae4e27a37ac22397be6522de29c
Yuan Sun <forrest.sun527@gmail.com>
2014-06-16 09:37:39 -0400

    Finished class source.

--- Source.java
+++ Source.java
@@ -1,33 +1,170 @@
-
 /**
  * Write a description of class Source here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Yuan Sun
+ * @version 1.0
  */
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class Source
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    private Document dom;
+    private NodeList nl;
+    private Element eElement;
+    private Element docEle;
+    private Node nNode;
+    private int maxID;
+    
     /**
      * Constructor for objects of class Source
      */
     public Source()
     {
-        // initialise instance variables
-        x = 0;
+        ParseSource();
+        maxID = getSourceCount();
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    
+    public void ParseSource()
     {
-        // put your code here
-        return x + y;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            dom = db.parse("database.xml");
+            docEle = dom.getDocumentElement();
+            nl = docEle.getElementsByTagName("Sources");
+            nNode = nl.item(0);
+            eElement = (Element) nNode;
+        }catch(ParserConfigurationException | SAXException | IOException e) {
+            System.out.println(e);
+        }
+    }
+    
+    public String getSourceName(int id)
+    {
+        if(id > 0 && id <= maxID)
+            id--;
+        else
+            return null;
+        return eElement.getElementsByTagName("name").item(id).getTextContent();
+    }
+    
+    public boolean setSourceName(int id,String name)
+    {
+        if(id <= 0 || id > maxID || name.length() < 1)
+            return false;
+        else {
+            id--;
+            eElement.getElementsByTagName("name").item(id).setTextContent(name);
+            try {
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(dom);
+                StreamResult file = new StreamResult(new File("database.xml"));
+                transformer.transform(source, file);
+                return true;
+            }catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+    
+    public String getSourceLink(int id)
+    {
+        if(id > 0 && id <= maxID)
+            id--;
+        else
+            return null;
+        return eElement.getElementsByTagName("link").item(id).getTextContent();
+    }
+    
+    public boolean setSourceLink(int id,String link)
+    {
+        if(id <= 0 || id > maxID || link.length() < 1)
+            return false;
+        else {
+            id--;
+            eElement.getElementsByTagName("link").item(id).setTextContent(link);
+            try {
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(dom);
+                StreamResult file = new StreamResult(new File("database.xml"));
+                transformer.transform(source, file);
+                return true;
+            }catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+    
+    public boolean addSource(String name,String link)
+    {
+        try {
+            if(name.length() > 0 && link.length() > 0) {
+                Element sourceNode = dom.createElement("source");
+                Element nameNode = dom.createElement("name");
+                Element linkNode = dom.createElement("link");
+                sourceNode.setAttribute("id", Integer.toString(getSourceCount()+1));
+                nameNode.setTextContent(name);
+                linkNode.setTextContent(link);
+                sourceNode.appendChild(nameNode);
+                sourceNode.appendChild(linkNode);
+                nNode.appendChild(sourceNode);
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                DOMSource source = new DOMSource(dom);
+                StreamResult file = new StreamResult(new File("database.xml"));
+                transformer.transform(source, file);
+                return true;
+            }
+            else
+                return false;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public boolean removeSource(int id)
+    {
+        try {
+            if(id > 0 && id <= maxID) {
+                id--;
+                nNode.removeChild(eElement.getElementsByTagName("link").item(id).getParentNode());
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(dom);
+                StreamResult file = new StreamResult(new File("database.xml"));
+                transformer.transform(source, file);
+                return true;
+            }
+            else
+                return false;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public int getSourceCount()
+    {
+        return eElement.getElementsByTagName("source").getLength();
     }
 }
