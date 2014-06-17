2476c9f3b604ae5e5d4d3180b90d3aeaaa81c6ee
Yuan Sun <forrest.sun527@gmail.com>
2014-06-16 20:13:34 -0400

    Done implementing class Category.

--- Category.java
+++ Category.java
@@ -1,33 +1,113 @@
-
 /**
- * Write a description of class Category here.
+ * Write a description of class Location here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Group 6
+ * @version 1.0
  */
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class Category
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    private Document dom;
+    private NodeList nl;
+    private Element eElement;
+    private Element docEle;
+    private Node nNode;
+    private int maxID;
+    
     /**
      * Constructor for objects of class Category
      */
     public Category()
     {
-        // initialise instance variables
-        x = 0;
+        ParseCategory();
+        maxID = getCategoryCount();
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    
+    public void ParseCategory()
     {
-        // put your code here
-        return x + y;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            dom = db.parse("database.xml");
+            docEle = dom.getDocumentElement();
+            nl = docEle.getElementsByTagName("Categories");
+            nNode = nl.item(0);
+            eElement = (Element) nNode;
+        }catch(ParserConfigurationException | SAXException | IOException e) {
+            System.out.println(e);
+        }
+    }
+    
+    public int searchCategory(String name)
+    {
+        if(name.length() > 0) {
+            for(int i = 0;i < maxID;i++) {
+                if(name.equals(eElement.getElementsByTagName("name").item(i).getTextContent()))
+                    return i+1;
+            }
+            return -1;
+        }
+        else
+            return -1;
+    }
+    
+    public ArrayList<Integer> getNewsFromCategory(String name)
+    {
+        int index = searchCategory(name);
+        ArrayList<Integer> newsList = new ArrayList<>();
+        if(index != -1) {
+            index--;
+            String[] news = eElement.getElementsByTagName("ids").item(index).getTextContent().split(",");
+            for(int i = 0;i < news.length;i++) {
+                newsList.add(Integer.parseInt(news[i]));
+            }
+        }
+        return newsList;
+    }
+    
+    public boolean addNewsToCategory(int id,String name)
+    {
+        int index = searchCategory(name);
+        if(id > 0 && index != -1) {
+            index--;
+            if(eElement.getElementsByTagName("ids").item(index).getTextContent().isEmpty())
+                eElement.getElementsByTagName("ids").item(index).setTextContent(Integer.toString(id));
+            else {
+                eElement.getElementsByTagName("ids").item(index).setTextContent(eElement.getElementsByTagName("ids").item(index).getTextContent() + "," + Integer.toString(id));
+            }
+            try {
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(dom);
+                StreamResult file = new StreamResult(new File("database.xml"));
+                transformer.transform(source, file);
+                return true;
+            }catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        else
+            return false;
+    }
+    
+    public int getCategoryCount()
+    {
+        return eElement.getElementsByTagName("category").getLength();
     }
 }
